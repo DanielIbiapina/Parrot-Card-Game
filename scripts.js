7fab28ebb5ac204e251fbb8ae39e6d2753030186
DanielIbiapina <111034429+DanielIbiapina@users.noreply.github.com>
2022-08-22 17:10:24 -0300

    Add files via upload
    Jogo completo. Falta somente adicionar a versão mobile e em quantas jogadas a pessoa concluiu o jogo.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 promptInicial();
 
 
-
+let firstCard = '';
+let secondCard = '';
 
 function promptInicial(){
     const quantasCartas = prompt('Com quantas cartas deseja jogar?');
@@ -28,24 +29,78 @@ function promptInicial(){
         'unicornparrot'
         
     ]
+    
+    cards.length = Number(quantasCartas);
+    console.log(cards);
     const cardsEmbaralhadas = cards.sort(comparador);
-    function comparador() { 
-        return Math.random() - 0.5; 
-    }
-   
     //ideia para amanha: duplicar o ul.innerHTML
-        
+    
+    
 
     for (let indice = 0; indice < Number(quantasCartas); indice++) {
+       
         ul.innerHTML += `<li>
-        <div class="carta abd" onclick ="virarCarta(this)">
+        <div class="carta ${cardsEmbaralhadas[indice]}" >
         <div class = "face frente"> <img src ="${cardsEmbaralhadas[indice]}.gif"/></div>
-        <div class = "face costas"></div>
+        <div class = "face costas" onclick="revealCard(this)" setAt></div>
         </div>
         </li>`;
+
+        
       }
+       
+     
+      
+     
+     
+      
+
+        
+    
+      function comparador() { 
+          return Math.random() - 0.5; 
+      }
+      
 } 
 
+function revealCard(elemento){
+   
+   if(elemento.className.includes('reveal-card')){
+    return;
+   }
+    if(firstCard == ''){
+        
+        elemento.classList.add('reveal-card');
+        firstCard = elemento.parentNode;
+        firstCardFilho = elemento;
+    } else if(secondCard == ''){
+        elemento.classList.add('reveal-card');
+        secondCard = elemento.parentNode;
+        secondCardFilho = elemento;
+    }
+    
+    if(firstCard.classList[1] == secondCard.classList[1]){
+        
+        firstCard = '';
+        secondCard = '';
+
+    } else{
+        setTimeout(intervalo,1000);
+        function intervalo(){
+            firstCardFilho.classList.remove('reveal-card');
+            secondCardFilho.classList.remove('reveal-card');
+            
+            firstCard = '';
+            secondCard = '';
+       }
+           
+            
+        
+    }
+    console.log(firstCard.classList[1]);
+    console.log(secondCard.classList[1]);
+    
+  }
 
 
 
