298f750fbcab9739037576233c12d6c43c1ddc2c
DanielIbiapina <111034429+DanielIbiapina@users.noreply.github.com>
2022-08-22 01:36:42 -0300

    Add files via upload
    Função de gerar cartas de acordo com o pedido está pronta, assim como a parte inicial de virar as cartas. Ainda falta alguns detalhes na função de embaralhar.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 promptInicial();
 
+
+
+
 function promptInicial(){
     const quantasCartas = prompt('Com quantas cartas deseja jogar?');
     if(quantasCartas !== '4' && quantasCartas !== '6' && quantasCartas !== '8' && quantasCartas !== '10' 
@@ -8,13 +11,46 @@ function promptInicial(){
         quantasCartas = 0;
     }
     const ul = document.querySelector("ul");
+    const cards = [
+        'bobrossparrot',
+        'bobrossparrot',
+        'explodyparrot',
+        'explodyparrot',
+        'fiestaparrot',
+        'fiestaparrot',
+        'metalparrot',
+        'metalparrot',
+        'revertitparrot',
+        'revertitparrot',
+        'tripletsparrot',
+        'tripletsparrot',
+        'unicornparrot',
+        'unicornparrot'
+        
+    ]
+    const cardsEmbaralhadas = cards.sort(comparador);
+    function comparador() { 
+        return Math.random() - 0.5; 
+    }
+   
+    //ideia para amanha: duplicar o ul.innerHTML
+        
 
     for (let indice = 0; indice < Number(quantasCartas); indice++) {
-        ul.innerHTML += `
-          <li><div class="carta"></div></li>
-        `;
+        ul.innerHTML += `<li>
+        <div class="carta abd" onclick ="virarCarta(this)">
+        <div class = "face frente"> <img src ="${cardsEmbaralhadas[indice]}.gif"/></div>
+        <div class = "face costas"></div>
+        </div>
+        </li>`;
       }
 } 
+
+
+
+
+
+
 
 
   
